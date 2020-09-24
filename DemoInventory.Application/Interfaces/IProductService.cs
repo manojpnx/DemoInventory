39cb62de57694b64f1dcead201dd5041b73c8445
Manoj Nandasiri <mpnandasiri@gowireless.com>
2020-09-25 01:34:58 +0530

    Project created

---
+++ DemoInventory.Application/Interfaces/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using DemoInventory.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoInventory.Application.Interfaces
+{
+    public interface IProductService
+    {
+        ProductViewModel GetProducts();
+    }
+}
---
+++ DemoInventory.Infrastructure.Data/Context/InventoryDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DemoInventory.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoInventory.Infrastructure.Data.Context
+{
+    public class InventoryDbContext : DbContext
+    {
+        public InventoryDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
